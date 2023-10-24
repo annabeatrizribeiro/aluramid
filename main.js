d0f1ddd644455b3871d03de19eb70078d9d86f3c
annabeatrizribeiro <127161058+annabeatrizribeiro@users.noreply.github.com>
2023-10-23 21:52:59 -0300

    alterandocodigo

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ function somTocaGrilo (){
 document.querySelector("#som_tecla_grilo").play();
 }
 document.querySelector(".tecla_grilo").onclick =somTocaGrilo;
+conset listaDeTeclas = document.querySelectorAll(".tecla");
+listaDeTeclas[4].onclick = tocaSomGrilo;
